116e558d2e178aa610805b035b1bffed6c62f220
jpajuelo <jpajuelogibaja@gmail.com>
2025-02-23 16:31:26 -0500

    Update JSON Mapping to Class

--- src/main/java/com/jpajuelo/products/entities/Product.java
+++ src/main/java/com/jpajuelo/products/entities/Product.java
@@ -1,25 +1,43 @@
 package com.jpajuelo.products.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @JsonProperty("id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @JsonProperty("name")
     private String name;
-
+    @JsonProperty("price")
     private Double price;
-    @Column(name = "create_at")
-    private LocalDate createAt;
+
+    @JsonProperty("create_at")
+    @Column(name = "create_at", updatable = false)
+    private LocalDateTime createAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", createAt=" + createAt +
+                '}';
+    }
 }
